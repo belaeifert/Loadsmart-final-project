242e667e55e13a14cbab8f9bd022262675df2e4a
belaeifert <bela.eifert@gmail.com>
2019-03-13 22:06:19 -0300

    initial commit

--- finalProject/api/urls.py
+++ finalProject/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from finalProject.api import views
 
 urlpatterns = [
-    path('loads/', views.LoadList.as_view()),
-    path('loads/<int:pk>/', views.LoadDetail.as_view()),
+    path('loads/', views.LoadList.as_view(), name='loads'),
+    path('loads/<int:pk>/', views.LoadDetail.as_view(), name='load_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
