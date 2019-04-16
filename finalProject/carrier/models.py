c917d8cb36bf4502ab5467034fdc5d4f4c4c0e3a
belaeifert <bela.eifert@gmail.com>
2019-04-16 14:47:51 -0300

    fix

--- finalProject/carrier/models.py
+++ finalProject/carrier/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from finalProject.account.models import User
-from finalProject.shipper.models import Load, ShipperUser
+from finalProject.shipper.models import Load
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -8,7 +8,7 @@ from django.utils.translation import ugettext_lazy as _
 class CarrierUser(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='carrier_user')
-    MC_number = models.IntegerField(_('MC number'), null=False, blank=False)
+    MC_number = models.IntegerField(_('MC number'), max_length=8, null=False, blank=False)
 
 
 class RejectedLoad(models.Model):
