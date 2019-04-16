463e886508f1cd1ed73bc0a8516aab18139df998
belaeifert <bela.eifert@gmail.com>
2019-04-16 15:42:57 -0300

    success message working

--- finalProject/shipper/views.py
+++ finalProject/shipper/views.py
@@ -6,7 +6,6 @@ from django.views import generic
 from finalProject.shipper.forms import LoadForm
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 
 
 @login_required
