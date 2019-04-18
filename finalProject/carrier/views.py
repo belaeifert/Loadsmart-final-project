856d34ea0556f6b7ee2c90fd8ca6ca5f0f2a0167
belaeifert <bela.eifert@gmail.com>
2019-04-18 16:55:05 -0300

    resolve accept load bug

--- finalProject/carrier/views.py
+++ finalProject/carrier/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.shortcuts import render, redirect
 from finalProject.shipper.models import Load
 from finalProject.carrier.models import RejectedLoad, CarrierUser
@@ -6,15 +7,13 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def list_loads(request):
-    pk_carrier = request.user.id
-    print("**********************************************")
-    print(pk_carrier)
+    carrier = CarrierUser.objects.get(user_id=request.user.id)
 
-    rej_loads = RejectedLoad.objects.filter(carrier_id=pk_carrier)
+    rej_loads = RejectedLoad.objects.filter(carrier=carrier)
     available_loads = Load.objects.filter(status='available').exclude(
         id__in=rej_loads.values('load_id'))
     accepted_loads = Load.objects.filter(
-        status='accepted', carrier_id=pk_carrier)
+        status='accepted', carrier=carrier)
     rejected_loads = Load.objects.filter(id__in=rej_loads.values('load_id'))
 
     return render(request, 'carrier_abas.html', {
@@ -22,10 +21,8 @@ def list_loads(request):
         'accepted_loads': accepted_loads,
         'rejected_loads': rejected_loads})
 
-
+@transaction.atomic
 def accept_load(request, pk_load):
-    print("****************************************8*******")
-    print(CarrierUser.objects.get(user_id=request.user.id))
     carrier = CarrierUser.objects.get(user_id=request.user.id)
     load = Load.objects.get(pk=pk_load)
     load.status = 'accepted'
@@ -33,7 +30,7 @@ def accept_load(request, pk_load):
     load.save()
     return redirect('carrier:list_loads')
 
-
+@transaction.atomic
 def reject_load(request, pk_load):
     load = Load.objects.get(pk=pk_load)
     carrier = CarrierUser.objects.get(user_id=request.user.id)
@@ -41,14 +38,14 @@ def reject_load(request, pk_load):
         load=load, carrier=carrier)
     return redirect('carrier:list_loads')
 
-
+@transaction.atomic
 def drop_load(request, pk_load):
     load = Load.objects.get(pk=pk_load)
     load.status = 'available'
     load.carrier = None
     load.save()
 
-    carrier = CarrierUser.objects.get(pk=request.user.id)
+    carrier = CarrierUser.objects.get(user_id=request.user.id)
 
     drop_load = RejectedLoad.objects.create(
         load=load, carrier=carrier)
