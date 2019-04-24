4676317abfa9cf01524b421f8131cf9f8e205375
alinekraemer <aline.kraemer@gmail.com>
2019-04-24 16:09:04 -0300

    add test for user model in app account

--- finalProject/account/tests.py
+++ finalProject/account/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from finalProject.account.models import User
+import unittest
 
-# Create your tests here.
+
+class UserModelTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(
+            email='carrier@teste.com', first_name='carrier', last_name='doe', password='teste1234')
+
+    def test_user_creation(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_carrier_atribute(self):
+        self.assertEqual(self.user.email, 'carrier@teste.com')
+
+
+if __name__ == '__main__':
+    unittest.main()
