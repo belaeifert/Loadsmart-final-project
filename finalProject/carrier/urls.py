1f9c40245780dc4a93131750ee7551b8919ba573
alinekraemer <aline.kraemer@gmail.com>
2019-05-02 15:08:00 -0300

    carrier with class based views

--- finalProject/carrier/urls.py
+++ finalProject/carrier/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "carrier"
 
 urlpatterns = [
-    path('home/', views.listLoads, name='home'),
-    path('home/load/accept/<int:pk_load>/', views.acceptLoad, name='accept_load'),
-    path('home/load/reject/<int:pk_load>/', views.rejectLoad, name='reject_load'),
-    path('home/load/drop/<int:pk_load>/', views.dropLoad, name='drop_load'),
+    path('home/', views.CarrierHome.as_view(), name='home'),
+    path('home/load/accept/<int:pk_load>/', views.AcceptLoad.as_view(), name='accept_load'),
+    path('home/load/reject/<int:pk_load>/', views.RejectLoad.as_view(), name='reject_load'),
+    path('home/load/drop/<int:pk_load>/', views.DropLoad.as_view(), name='drop_load'),
 ]
